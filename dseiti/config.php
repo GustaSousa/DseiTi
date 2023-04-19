a79d4347a5c75e5870dc5267f192cb5c2c01ecc7
GustaSousa <sktgusta@gmail.com>
2023-04-19 16:15:09 -0300

    first commit

---
+++ dseiti/config.php
@@ -0,0 +1,9 @@
+<?php
+
+    $dbHost = 'Localhost';
+    $dbUsername =  'root';
+    $dbPassword = '';
+    $dbName = 'dseitidb';
+
+    $conexao = new mysqli($dbHost,$dbUsername,$dbPassword,$dbName);
+?>
